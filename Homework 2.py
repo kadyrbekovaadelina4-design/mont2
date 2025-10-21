82125083b0ca22a07cf394ba2213c9dcfcef7d73
kadyrbekovaadelina4-design <kadyrbekovaadelina4@gmail.com>
2025-10-21 19:39:39 +0600

    hw2

--- Homework 2.py
+++ Homework 2.py
@@ -3,10 +3,10 @@ from bisect import bisect_right
 
 class Person:
     def __init__(self,name,brith_date,occupation,higher_edukation):
-      self.name = name
-      self.brith_date = brith_date
-      self.occupation = occupation
-      self.higher_edukation = higher_edukation
+        self.name = name
+        self.brith_date = brith_date
+        self.occupation = occupation
+        self.higher_edukation = higher_edukation
 
     def introduce(self):
         return(f'Имя - {self.name}\n'
@@ -21,11 +21,35 @@ print(people1.introduce())
 
 class Friend(Person):
     def __init__(self,name,brith_date,occupation,higher_edukation, time_fried):
-        Person.__init__(name, brith_date, occupation, higher_edukation)
+        Person.__init__(self, name, brith_date, occupation, higher_edukation,)
         self.time_friend = time_fried
+
+    def introduce(self):
+        return (f'Имя - {self.name}\n'
+                f'День рождения - {self.brith_date}\n'
+                f'Занятия - {self.occupation}\n'
+                f'Высшее_образование - {self.higher_edukation}\n'
+                f'Сколько лет дружим - {self.time_friend}')
 
 
 class Classmate(Person):
     def __init__(self, name, brith_date, occupation, higher_edukation, status):
-        Person.__init__(name, brith_date, occupation, higher_edukation)
+        Person.__init__(self, name, brith_date, occupation, higher_edukation,)
         self.status = status
+
+    def introduce(self):
+        return (f'Имя - {self.name}\n'
+                f'День рождения - {self.brith_date}\n'
+                f'Занятия - {self.occupation}\n'
+                f'Высшее_образование - {self.higher_edukation}\n'
+                f'Статус в классе {self.status}')
+
+fr1 = Friend('Бектан', 2008, 'football', 'среднее', 7)
+fr2 = Friend('Султан', 2009, 'бокс', 'среднее', 5)
+print(fr1.introduce())
+print(fr2.introduce())
+
+cl1 = Classmate('Марьям', 2008, 'model', 'среднее', 'отличница')
+cl2 = Classmate('Улукбек', 2008, 'math', 'среднее неполное','отличник' )
+print(cl1.introduce())
+print(cl2.introduce())
